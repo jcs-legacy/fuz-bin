256c92a892df1021c2261e485a3d7c9078e295d1
Jen-Chieh Shen <jcs090218@gmail.com>
2021-10-16 16:24:34 +0800

    Add core

---
+++ fuz-bin.el
@@ -0,0 +1,81 @@
+;;; fuz-bin.el --- Fast and precise fuzzy scoring/matching utils  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Shen, Jen-Chieh
+;; Created date 2021-10-15 01:03:01
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Fast and precise fuzzy scoring/matching utils.
+;; Keyword: lisp
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "25.1"))
+;; URL: https://github.com/jcs-elpa/fuz-bin
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Fast and precise fuzzy scoring/matching utils.
+;;
+
+;;; Code:
+
+(require 'cl-lib)
+
+(defconst fuz-bin--bin-dir
+  (concat (file-name-directory load-file-name) "bin/")
+  "Pre-built binaries directory path.")
+
+(defconst fuz-bin--dyn-name "fuz_bin_dyn"
+  "Dynamic module name.")
+
+;;
+;; (@* "Externals" )
+;;
+
+(declare-function fuz-bin-dyn-calc-score-clangd "fuz-bin-dyn")
+(declare-function fuz-bin-dyn-calc-score-skim "fuz-bin-dyn")
+(declare-function fuz-bin-dyn-find-indices-clangd "fuz-bin-dyn")
+(declare-function fuz-bin-dyn-find-indices-skim "fuz-bin-dyn")
+
+;;
+;; (@* "Aliases" )
+;;
+
+(defalias 'fuz-bin-calc-score-clangd #'fuz-bin-dyn-calc-score-clangd)
+(defalias 'fuz-bin-calc-score-skim #'fuz-bin-dyn-calc-score-skim)
+(defalias 'fuz-bin-find-indices-clangd #'fuz-bin-dyn-find-indices-clangd)
+(defalias 'fuz-bin-find-indices-skim #'fuz-bin-dyn-find-indices-skim)
+
+
+;;
+;; (@* "Bootstrap" )
+;;
+
+;;;###autoload
+(defun fuz-bin-load-dyn ()
+  "Load dynamic module."
+  (interactive)
+  (unless (featurep 'fuz-bin-dyn)
+    (let* ((dyn-name (cl-case system-type
+                       ((windows-nt ms-dos cygwin) (concat fuz-bin--dyn-name ".dll"))
+                       (darwin (concat "lib" fuz-bin--dyn-name ".dylib"))
+                       (t (concat "lib" fuz-bin--dyn-name ".so"))))
+           (dyn-path (concat fuz-bin--bin-dir dyn-name)))
+      (module-load dyn-path)
+      (message "[INFO] Successfully load dynamic module, `%s`" dyn-name))))
+
+(provide 'fuz-bin)
+;;; fuz-bin.el ends here
